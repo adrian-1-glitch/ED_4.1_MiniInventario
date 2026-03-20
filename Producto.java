e4decd4f50e13df566595c4122870c55b836e961
adrian-1-glitch <nasriadrian1digitechfp@gmail.com>
2026-03-20 07:51:09 +0100

    Add Producto class with attributes and methods

---
+++ Producto.java
@@ -0,0 +1,45 @@
+public class Producto {
+    //ATRIBUTOS PRIVADOS
+    private String codigo, nombre, categoria;
+    private double precio;
+    private int stock;
+    //CONSTRUCTORES
+    public Producto(String codigo, String nombre, String categoria,double precio, int stock) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+        this.stock = stock;
+    }
+    //GETTERS Y SETTERS
+    public String getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public double getPrecio() {
+        return precio;
+    }
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    public int getStock() {
+        return stock;
+    }
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
